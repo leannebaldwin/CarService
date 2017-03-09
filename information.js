0613ae701764346b06cff44d10d49906203b3cf7
Leanne Baldwin <leanne@reefbreak.co.uk>
2017-03-09 15:48:20 -0800

    Create information.js

---
+++ information.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "service": "Your Mazda is due for a service in 8500 miles, your last service was 4 months ago.",
+    "car health": "Your Mazda currently has no warning lights and all tyre pressures are within recommended limits.",
+    "motorsport": "Jason, it's Mazda motorsport weekend, why not visit us and demo the C X 9, say book now to reserve a slot.",
+    "book now": "We have sent you an email",
+    "location": "Jason's Mazda is currently parked at Jason's office"
+    };
